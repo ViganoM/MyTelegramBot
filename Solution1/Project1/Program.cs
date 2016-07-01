2a77a71b26015b4b61f0b8e5c3fce0589710f82f
Michele Viganò <vigano_m@outlook.com>
2016-07-01 11:12:53 +0200

    update

--- Solution1/Project1/Program.cs
+++ Solution1/Project1/Program.cs
@@ -8,14 +8,22 @@ using System.Web.UI;
 using System.Web.Script.Serialization;
 using System.IO;
 using System.Diagnostics;
-
+using System.Threading;
+using JsonTypes;
 namespace Project1 {
     class Program {
 
         static void Main(string[] args) {
-            Console.Write(ServerMethods.GetMe());
+            if ( ServerMethods.GetMe().ok )
+                new Program().Updater();
             Console.ReadKey();
-
+        }
+        void Updater() {
+            while ( true ) {
+                foreach ( Update update in ServerMethods.GetUpdates().result )
+                    ServerMethods.sendMessage(update.message.chat.id, update.message.text + "\n<b>MV</b>",ServerMethods.parse_mode.HTML);
+                Thread.Sleep(100);
+            }
         }
     }
 }
--- Solution1/Project1/ServerMethods.cs
+++ Solution1/Project1/ServerMethods.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using JsonTypes;
@@ -15,13 +14,71 @@ namespace Project1 {
         const string token = "225365907:AAE0D5dgDjzLlwHp5jLVVZMBmRZyPBFpepI";
         const string website = "https://api.telegram.org/bot"+token+"/";
 
+        static Dictionary<int,int> lastUpdate = new Dictionary<int, int>();
+
+        public enum parse_mode { Markdown, HTML }
+
+        static public getUpdates GetUpdates(int limit = 100) {
+            string argument = string.Format("?limit={0}", limit);
+            return GetUpdates(argument);
+        }
+        static public getUpdates GetUpdates(int offset, int limit = 100) {
+            string argument = string.Format("?offset={0}&limit={1}", offset, limit);
+            return GetUpdates(argument);
+        }
+        static public getUpdates GetUpdatesbyChat(int chat_id, int limit=100) {
+            string argument = string.Format("?limit={0}", limit);
+            getUpdates getUpdates = GetUpdates(argument).result;
+            for (int n=getUpdates.result.cou
+
+        } 
+        static public getUpdates GetLastUpdatebyChat(int chat_id) {
+
+        }
+        static getUpdates GetUpdates(string argument) {
+            string response = new WebClient().DownloadString(website + "getUpdates" + argument);
+            getUpdates getUpdates = new JavaScriptSerializer().Deserialize<getUpdates>(response);
+            foreach ( Update update in getUpdates.result ) {
+                if ( !lastUpdate.ContainsKey(update.message.chat.id) )
+                    lastUpdate.Add(update.message.chat.id, update.update_id);
+                if ( update.update_id > lastUpdate[update.message.chat.id] )
+                    lastUpdate[update.message.chat.id] = update.update_id;
+            }
+            return getUpdates;
+        }
+
         static public getMe GetMe() {
             string response = new WebClient().DownloadString(website + "getMe");
             return new JavaScriptSerializer().Deserialize<getMe>(response);
         }
-        static public getUpdates GetUpdates() {
-            string response = new WebClient().DownloadString(website + "getUpdates");
-            return new JavaScriptSerializer().Deserialize<getUpdates>(response);
+
+        static public Message sendMessage(int chat_id, string text, int reply_to_message_id = -1) {
+            string argument = string.Format("?chat_id={0}&text={1}", chat_id, text);
+            if ( reply_to_message_id != -1 )
+                argument += "&reply_to_message_id=" + reply_to_message_id;
+            return sendMessage(argument);
+        }
+        static public Message sendMessage(int chat_id, string text, parse_mode parse_mode, int reply_to_message_id = -1) {
+            string argument = string.Format("?chat_id={0}&text={1}&parse_mode={2}", chat_id, text, parse_mode);
+            if ( reply_to_message_id != -1 )
+                argument += "&reply_to_message_id=" + reply_to_message_id;
+            return sendMessage(argument);
+        }
+        static public Message sendMessage(int chat_id, string text, bool disable_notification, int reply_to_message_id = -1) {
+            string argument = string.Format("?chat_id={0}&text={1}&disable_notification={2}", chat_id, text, disable_notification);
+            if ( reply_to_message_id != -1 )
+                argument += "&reply_to_message_id=" + reply_to_message_id;
+            return sendMessage(argument);
+        }
+        static public Message sendMessage(int chat_id, string text, parse_mode parse_mode, bool disable_notification, int reply_to_message_id = -1) {
+            string argument = string.Format("?chat_id={0}&text={1}&parse_mode={2}&disable_notification={3}", chat_id, text, parse_mode, disable_notification);
+            if ( reply_to_message_id != -1 )
+                argument += "&reply_to_message_id=" + reply_to_message_id;
+            return sendMessage(argument);
+        }
+        static Message sendMessage(string argument) {
+            string response = new WebClient().DownloadString(website + "sendMessage" + argument);
+            return new JavaScriptSerializer().Deserialize<Message>(response);
         }
     }
 }
