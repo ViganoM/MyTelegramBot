18a0f10e074a3c6332861a415ecb9aa4f65b0d55
Michele Viganò <vigano_m@outlook.com>
2016-07-01 09:29:07 +0200

    update

--- Solution1/Project1/Program.cs
+++ Solution1/Project1/Program.cs
@@ -3,10 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Net;
+using System.Web.UI;
+using System.Web.Script.Serialization;
+using System.IO;
+using System.Diagnostics;
 
 namespace Project1 {
     class Program {
+
         static void Main(string[] args) {
+            Console.Write(ServerMethods.GetMe());
+            Console.ReadKey();
+
         }
     }
 }
---
+++ Solution1/Project1/ServerMethods.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using JsonTypes;
+using System.Web;
+using System.Net;
+using System.Web.Script.Serialization;
+
+
+namespace Project1 {
+    class ServerMethods {
+
+        const string token = "225365907:AAE0D5dgDjzLlwHp5jLVVZMBmRZyPBFpepI";
+        const string website = "https://api.telegram.org/bot"+token+"/";
+
+        static public getMe GetMe() {
+            string response = new WebClient().DownloadString(website + "getMe");
+            return new JavaScriptSerializer().Deserialize<getMe>(response);
+        }
+        static public getUpdates GetUpdates() {
+            string response = new WebClient().DownloadString(website + "getUpdates");
+            return new JavaScriptSerializer().Deserialize<getUpdates>(response);
+        }
+    }
+}
